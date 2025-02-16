0fd676cccc3d287fcbcbf874b79ef577ce4ee8a8
Fernando Lamas <lamas.fernando1419@gmail.com>
2025-02-16 20:12:05 -0300

    Add Logout component

---
+++ src/auth/Logout.vue
@@ -0,0 +1,12 @@
+<template>
+   <div>
+      <button @click="logout">Logout</button>
+   </div>
+</template>
+
+<script setup>
+const logout = () => {
+   localStorage.removeItem('authToken');
+   window.location.href = '/login'; // Redirect to login page
+};
+</script>

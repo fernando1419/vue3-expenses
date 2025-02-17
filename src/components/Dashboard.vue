9293317e1a926eb23459d3c897da36e363e13c1c
Fernando Lamas <lamas.fernando1419@gmail.com>
2025-02-16 22:10:45 -0300

    Add dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<script setup>
+const handleLogout = () => {
+   window.location.href = '/dashboard';
+};
+</script>
+
+<template>
+   <div>
+      <h2>Dashboard</h2>
+      <p>Welcome, user: {{ }}</p>
+      <button @click="handleLogout">Logout</button>
+   </div>
+</template>
